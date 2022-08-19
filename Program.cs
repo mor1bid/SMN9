b443c9aedd70abee0adbad76fe6a6f7da8bf6d27
De <spark49942@gmail.com>
2022-08-19 20:00:01 +0300

    Z1

--- Program.cs
+++ Program.cs
@@ -16,6 +16,6 @@ string Rec(int a, int b )
     if(a<=b) return $"{a}" + Rec(a+1, b);
     else return String.Empty;
 }
-Console.WriteLine(Rec(1,10) + "\t");
+Console.WriteLine(Rec(1,10) + " ");
 }
 Z1();
