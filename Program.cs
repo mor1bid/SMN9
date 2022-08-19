5d1da6abd277378a202c25da7c92cb5c0b77fdb7
De <spark49942@gmail.com>
2022-08-20 00:06:29 +0300

    DZ12

--- Program.cs
+++ Program.cs
@@ -1,54 +1,85 @@
-﻿void Z0() 
+﻿void Z3() 
 {
-Console.WriteLine("63.-65. Введите первое число:");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число:");
-int n = Convert.ToInt32(Console.ReadLine());
-void Rec(int num, int co )
+Console.WriteLine("67. Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+//int sum = 0;
+int Rec(int num, int sum = 0)
 {
-    if(co>num) return;
-    Console.WriteLine(co);
-    co++;
-    Rec(num,co);
+    if(num!=0)
+    {
+        sum+=num%10;
+        num/=10;
+        return Rec(num, sum);
+    }
+    else return sum;
 }
+Console.WriteLine(num + " = " + Rec(num));
 }
-//Z0();
+//Z3();
 
-void Z12() 
-{
-Console.WriteLine("63.-65. Введите первое число:");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число:");
-int n = Convert.ToInt32(Console.ReadLine());
-string Rec(int a, int b )
-{
-    if(a<=b) return $"{a} " + Rec(a+1, b);
-    else return String.Empty;
-}
-Console.WriteLine(Rec(m,n));
-}
-//Z12();
-
-void Z3() 
+void Z4() 
 {
 Console.WriteLine("67. Введите первое число:");
 int numa = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
+Console.WriteLine("Введите второе число:");
+int numb = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int Rec(int numa, int numb)
+{
+    while (i<numb) 
+    { 
+        numa*=numb; 
+        i++; 
+        return Rec(numa,numb); 
+    }
+    return numa;
+
+}
+Console.WriteLine(numa + " в степени " + numb + " = " + Rec(numa,numb));
+}
+//Z4();
+
+void DZ1() 
+{
+Console.WriteLine("64. Введите первое число:");
+int numa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
 int numb = Convert.ToInt32(Console.ReadLine());
 int co = numb;
 int Rec(int co, int numb) 
 {
-    while(co>=numa) 
+    while(co>numa) 
     {
         co--;
         if (co % 3 ==0) 
         {
-            Console.Write(co + " ");
+            //Console.Write(co + " ");
+            //Console.WriteLine(Rec(co,numb));
             return Rec(co,numb);
         }
-        return co;
     }
+    return co;
 }
-//Console.WriteLine(Rec(co,numb));
+Console.WriteLine(Rec(co,numb));
 }
-Z3();
+//DZ1();
+
+void DZ2() 
+{
+Console.WriteLine("66. Введите первое число:");
+int numa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+int Rec(int co, int numb, int sum = 0) 
+{
+    while(co>numa) 
+    {
+        co--;
+        sum+=co;
+        return Rec(co,sum);
+    }
+    return sum;
+}
+Console.WriteLine(Rec(numb, 0));
+}
+DZ2();
