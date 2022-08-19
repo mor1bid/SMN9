7c51360b6feaf5234fac9d5943a1eb229987ec8a
De <spark49942@gmail.com>
2022-08-19 20:48:24 +0300

    Z2

--- Program.cs
+++ Program.cs
@@ -15,10 +15,12 @@ Console.WriteLine("Введите число:");
 int n = Convert.ToInt32(Console.ReadLine());
 string Rec(int a, int b )
 {
-    if(a<=b) return $"{a}" + Rec(a+1, b);
+    Console.WriteLine("");
+    if(a<=b) 
+    { 
+        return $"{a}" + Rec(a+1, b); }
     else return String.Empty;
 }
-Console.WriteLine("");
 Console.WriteLine(Rec(1,n));
 }
 Z1();
