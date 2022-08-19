93d688cb47669212f3e6df758380d4a3fe254679
De <spark49942@gmail.com>
2022-08-19 19:59:25 +0300

    Z1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿void Z1() 
+﻿void Z0() 
 {
 string Rec(int a, int b )
 {
@@ -6,5 +6,16 @@ string Rec(int a, int b )
     else return String.Empty;
 }
 Console.WriteLine(Rec(1,10));
+}
+Z0();
+
+void Z1() 
+{
+string Rec(int a, int b )
+{
+    if(a<=b) return $"{a}" + Rec(a+1, b);
+    else return String.Empty;
+}
+Console.WriteLine(Rec(1,10) + "\t");
 }
 Z1();
