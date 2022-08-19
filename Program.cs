77480a63b69dc09d131bee3c4a470f6d002ba04c
De <spark49942@gmail.com>
2022-08-19 21:01:32 +0300

    Z2

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ Console.WriteLine(Rec(1,10));
 }
 //Z0();
 
-void Z1() 
+void Z12() 
 {
 Console.WriteLine("63.-65. Введите первое число:");
 int m = Convert.ToInt32(Console.ReadLine());
@@ -22,4 +22,18 @@ string Rec(int a, int b )
 }
 Console.WriteLine(Rec(m,n));
 }
-Z1();
+//Z12();
+
+void Z3() 
+{
+Console.WriteLine("67. Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+string Rec(int a, int b )
+{
+    if(a<=b) return $"{a}" + Rec(a+1, b);
+    else return String.Empty;
+}
+Console.WriteLine(Rec(sum+=num%10,num));
+}
+Z3();
